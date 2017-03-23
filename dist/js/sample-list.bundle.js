050fc63f564827352d773403d1be26ac43eba374
David Younker <david.younker@mediacurrent.com>
2017-03-23 15:11:13 -0400

    Add scss and js to component

---
+++ dist/js/sample-list.bundle.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function (Drupal, $) {
+
+  Drupal.behaviors.sampleList = {
+    attach: function attach(context, settings) {
+
+      console.log('JS is here');
+    }
+  };
+})(Drupal, jQuery);
+//# sourceMappingURL=sample-list.bundle.js.map
---
+++ src/components/sample-list/sample-list.js
@@ -0,0 +1,12 @@
+'use strict';
+(function (Drupal, $) {
+
+  Drupal.behaviors.sampleList = {
+    attach: function (context, settings) {
+
+      console.log('JS is here');
+
+    }
+  };
+})(Drupal, jQuery);
+
